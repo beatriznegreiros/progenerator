3a8ddd3f765d9ecabdb9e40e54d7982d6c6a5a15
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-07-29 09:49:09 +0200

    substituted code for a organized module progenerator.py, it is a standalone version

--- Querprofilvergleich/progenerator.py
+++ Querprofilvergleich/progenerator.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib.ticker as ticker
 
 
 # Practical Routines
@@ -16,7 +17,7 @@ def split_float(x):
     return int(before), (int(after) * 10 if len(after) == 1 else int(after))
 
 
-def profiles_generator(path_banks, list_of_profiles, markersize, marker, outputfolder):
+def profiles_generator(path_banks, list_of_profiles, header, markersize, marker, xspacing, yspacing, output_folder):
     corners = pd.read_csv(path_banks, skip_blank_lines=True, index_col=0, header=None)
     corners.dropna(how='any', inplace=True, axis=0)
     corners = rename_columns(corners)  # Standardize columns names
@@ -27,13 +28,15 @@ def profiles_generator(path_banks, list_of_profiles, markersize, marker, outputf
     # Iterates through km sections
     for sec in stamm_profiles:
         i = 0
+        f, ax = plt.subplots(figsize=(10, 6))
+
         for _file in list_of_profiles:
-            path_profile = str(current_dir) + '/' + _file + '.csv'
+
             # Take the Stamm Punkte
             reference_banks = corners.loc[sec]
 
             # Read the DataFrame
-            profile = pd.read_csv(path_profile, skip_blank_lines=True, index_col=0, header=None)
+            profile = pd.read_csv(_file, skip_blank_lines=True, index_col=0, header=header)
             profile.dropna(how='any', inplace=True, axis=0)
 
             # Standardize the columns
@@ -48,26 +51,27 @@ def profiles_generator(path_banks, list_of_profiles, markersize, marker, outputf
                 distances = (points_in_section.lat ** 2 + points_in_section.long ** 2) ** 0.5
                 references = np.array((reference_banks.lat ** 2 + reference_banks.long ** 2) ** 0.5)
 
-                # Boolan array to take only points inside the stamm punkte
+                # Boolean array to take only points inside the stamm punkte
                 isinside = (distances <= references[1]) & (distances >= references[0]) | ((distances <= references[0])
-                                                                                          & (distances >= references[1]))
+                                                                                          & (distances >= references[
+                            1]))
 
                 # Re-assign dataframes to take only points inside stammpunkte
-                distances = distances[isinside]
                 points_in_section = points_in_section[isinside]
 
                 # Calculates the relative distance of the bathymetry having as reference point the left bank
-                distances = abs(distances - references[0])
-                references = abs(references - references[0])
+                plot_distances = abs(((points_in_section.lat - reference_banks['lat'].iloc[0]) ** 2 + (
+                        points_in_section.long - reference_banks['long'].iloc[0]) ** 2) ** 0.5)
 
                 # Plots the km section
-                plt.plot(distances, points_in_section.bedelevation, marker=marker[i],
-                         markerfacecolor='black', markersize=markersize[i], label=_file)
+                ax.plot(plot_distances, points_in_section.bedelevation, marker=marker[i],
+                        markerfacecolor='black', markersize=markersize[i], label=titles[i])
 
-                plt.legend(loc='upper center')
+                ax.legend(loc='upper center')
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(xspacing))
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(yspacing))
 
-                # plt.scatter(references, reference_banks.bedelevation, marker="^", edgecolors='c', label='Stamm
-                # Punkte')
+                # plt.scatter(references, reference_banks.bedelevation, marker="^", edgecolors='c', label='Stamm Punkte')
                 i += 1
             except:  # skips the plot if the section doesnt exist
                 message = "Could not save plot for " + _file + " in km section " + str(sec)
@@ -76,14 +80,15 @@ def profiles_generator(path_banks, list_of_profiles, markersize, marker, outputf
 
         # Get title and path of the section figure
         integer, decimal = split_float(sec)
-        title = 'Cross section of km ' + str(sec)
-        plt.xlabel('Distance from left bank [m]')
-        plt.ylabel('Elevation [m.a.s.l]')
-        plt.title(title)
-        out = outputfolder + '/' + 'km_' + str(integer) + '_' + str(decimal)
+        ax.tick_params(axis='both', labelsize='large')
+        ax.set_xlabel(xlabel='Distance from left bank [m]')
+        ax.set_ylabel(ylabel='Elevation [m.a.s.l]')
+        # ax.tick_params(axis='x', width=20)
+        ax.set_title(label='Cross section of km ' + str(sec))
+        out = output_folder + '/' + 'km_' + str(integer) + '_' + str(decimal)
 
         # Save figure
-        plt.savefig(out)
+        f.savefig(out)
         plt.clf()
 
 
@@ -91,20 +96,35 @@ if __name__ == '__main__':
     current_dir = Path.cwd()
 
     # ------------- INPUT (without extension of file) -------------------
-    list_of_profiles = ['Inn_Profile_2014', 'QP_2007_Fkm', 'Querprofile_2020_final']  # Profiles must be in same folder
-    bank_limits = 'Inn_Stamm_2014'
-    markersize = [8, 4, 0]  # corresponding to the respective profiles
-    marker = [".", "^", "."]  # corresponding to the respective profiles
+
+    # Path for the profiles to be plotted
     # (km section, lat, long, bed elevation) obs.: first column is always 0
+    profiles = [str(current_dir) + '/' + 'Profiles_Mesh_2007' + '.csv',
+                str(current_dir) + '/' + 'QP_2007_Fkm' + '.csv']
 
-    name_output_folder = 'Inn_Profiles_2014_2007_2020'
+    # Path to stamm punkte
+    # (km section, lat, long, bed elevation) obs.: first column is always 0
+    bank_limits = str(current_dir) + '/' + 'Inn_Stamm_2014' + '.csv'
 
-    # Read Stamm punkte
+    # Titles of the legend in the plots
+    titles = ['Bed elevation at 2007 (Mesh)', 'Bed elevation at 2007 (Measurements)']
 
-    path_banks = str(current_dir) + '/' + bank_limits + '.csv'  # Path to stamm punkte
+    # Spacing for the x labels
+    xspacing = 20  # every 20 m
+    yspacing = 1  # every 1 m
+
+    # If the .csv files have header, please inser tthe number of header lines
+    header = None
+
+    markersize = [8, 4]  # corresponding to the respective profiles
+    marker = [".", "^"]  # corresponding to the respective profiles
+
+    # Folder to save all images
+    output_folder = str(current_dir) + '/' + 'Inn_Profiles_2007_mesh_vs_meas'
 
     # -------------------------------------------------------------------
-    output_folder = str(current_dir) + '/' + name_output_folder
 
     # Create folder if it doesnt exist
     Path(output_folder).mkdir(exist_ok=True)
+
+    profiles_generator(bank_limits, profiles, header, markersize, marker, xspacing, yspacing, output_folder)
