8c0f9c96528b698c015f9f4e1dd050d5d46f57a9
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2020-08-25 12:25:18 +0200

    initial steps for deleting outside points

--- Querprofilvergleich/progenerator.py
+++ Querprofilvergleich/progenerator.py
@@ -21,7 +21,7 @@ def split_float(x):
 
 
 def profiles_generator(path_banks, list_of_profiles, header, markersize, marker, titles, output_folder, xspacing,
-                       yspacing, only_points_inside=None):
+                       yspacing, delete_outside=None):
     """
     :param path_banks: str, text file in *.csv format, file which delimits the points inside a river profile
     :param list_of_profiles: list of str, text files in *.csv format for the profiles to be generated
@@ -53,7 +53,8 @@ def profiles_generator(path_banks, list_of_profiles, header, markersize, marker,
         f, ax = plt.subplots()
         ax.xaxis.set_major_locator(ticker.MultipleLocator(xspacing))
         ax.yaxis.set_major_locator(ticker.MultipleLocator(yspacing))
-
+        plt.grid(True)
+        
         for _file in list_of_profiles:
 
             # Take the Stamm Punkte
@@ -75,13 +76,31 @@ def profiles_generator(path_banks, list_of_profiles, header, markersize, marker,
                 distances = (points_in_section.lat ** 2 + points_in_section.long ** 2) ** 0.5
                 references = np.array((reference_banks.lat ** 2 + reference_banks.long ** 2) ** 0.5)
 
-                # Boolean array to take only points inside a certain range of interest
-                isinside = (distances <= references[1]) & (distances >= references[0]) | (
-                        (distances <= references[0])
-                        & (distances >= references[1]))
+                if delete_outside is None:
+                    # Boolean array to take only points inside a certain range of interest
+                    isinside = (distances <= references[1]) & (distances >= references[0]) | (
+                            (distances <= references[0])
+                            & (distances >= references[1]))
 
-                # Re-assign dataframes to take only points inside stammpunkte
-                points_in_section = points_in_section[isinside]
+                    # Re-assign dataframes to take only points inside stammpunkte
+                    points_in_section = points_in_section[isinside]
+
+                elif delete_outside is not None and delete_outside is not _file:
+                    delete_points_outside = pd.read_csv(delete_outside, skip_blank_lines=True, index_col=0,
+                                                        header=header)
+                    delete_points_outside.dropna(how='any', inplace=True, axis=0)
+                    delete_points_outside = rename_columns(delete_points_outside)
+                    delete_points_outside = delete_points_outside.loc[sec]
+                    dist_delete = (delete_points_outside.lat ** 2 + delete_points_outside.long ** 2) ** 0.5
+                    delete = np.array([np.max(dist_delete), np.min(dist_delete)])
+
+                    isinside = ((distances <= delete[1]) & (distances >= delete[0]) | (
+                            (distances <= delete[0])
+                            & (distances >= delete[1]))) & ((distances <= references[1]) & (distances >= references[0]) | (
+                            (distances <= references[0])
+                            & (distances >= references[1])))
+
+                    points_in_section = points_in_section[isinside]
 
                 # Calculates the relative distance of the bathymetry having as reference point the left bank
                 plot_distances = abs(((points_in_section.lat - reference_banks['lat'].iloc[0]) ** 2 + (
@@ -92,7 +111,7 @@ def profiles_generator(path_banks, list_of_profiles, header, markersize, marker,
                         markerfacecolor='black', markersize=markersize[i], label=titles[i])
 
                 ax.legend(loc='upper center')
-                #plt.xlim(0, 180)
+                # plt.xlim(0, 180)
 
                 # plt.scatter(references, reference_banks.bedelevation, marker="^", edgecolors='c', label='Stamm Punkte')
                 i += 1
@@ -123,15 +142,17 @@ if __name__ == '__main__':
 
     # Path for the profiles to be plotted
     # (km section, lat, long, bed elevation) obs.: first column is always 0
-    profiles = [str(current_dir) + '/' + 'Profiles_Mesh_2007' + '.csv',
-                str(current_dir) + '/' + 'QP_2007_Fkm' + '.csv']
+    profiles = [str(current_dir / 'Profiles_Mesh_2007') + '.csv',
+                str(current_dir / 'QP_2007_Fkm') + '.csv'
+                ]
 
     # Path to stamm punkte
     # (km section, lat, long, bed elevation) obs.: first column is always 0
     bank_limits = str(current_dir) + '/' + 'Inn_Stamm_2014' + '.csv'
 
     # Titles of the legend in the plots
-    title_list = ['Bed elevation in 2007 (Mesh)', 'Bed elevation in 2007 (Measurements)']
+    title_list = ['Bed elevation in 2007 (Mesh)',
+                  'Bed elevation in 2007 (Measurements)']
 
     # Spacing for the x labels
     del_x = 20  # every 20 m
@@ -144,12 +165,11 @@ if __name__ == '__main__':
     marker = [".", "^"]  # corresponding to the respective profiles
 
     # Folder to save all images
-    output_folder = str(current_dir) + '/' + 'Inn_Profiles_2007_mesh_vs_meas'
+    output_folder = str(current_dir) + '/' + 'Inn_Profiles_2007_mesh_vs_meas_02'
 
     # -------------------------------------------------------------------
 
     # Create folder if it doesnt exist
     Path(output_folder).mkdir(exist_ok=True)
-
     profiles_generator(bank_limits, profiles, header, markersize, marker, title_list, output_folder, xspacing=del_x,
-                       yspacing=del_y)
+                       yspacing=del_y, delete_outside=str(current_dir / 'Profiles_Mesh_2007') + '.csv')
